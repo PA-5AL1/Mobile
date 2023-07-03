e6a5470e7f5037eae1d66f1923ef182626b4adcb
Ifzas Nguyen <nguyen.ifzas@gmail.com>
2023-07-03 07:51:53 +0200

    fixed future builder state

--- lib/src/features/core/screens/profile/profile_screen.dart
+++ lib/src/features/core/screens/profile/profile_screen.dart
@@ -35,7 +35,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   Future<String> retrieveCurrentUserEmail() async {
     var user = await controller.currentUser;
-    _role = await userController.getUserData(user.email);
+    // _role = await userController.getUserData(user.email);
     return user.email;
   }
   bool _isDark = MediaQueryData.fromWindow(WidgetsBinding.instance.window)
@@ -49,117 +49,122 @@ class _ProfileScreenState extends State<ProfileScreen> {
       future: _email,
       builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
         List<Widget> children;
-        if (snapshot.hasData) {
-          return Scaffold(
-            body: SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.all(tDefaultSize),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: 120,
-                      height: 120,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: const Image(
-                          image: AssetImage(tProfileImage),
+        if(snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            return Scaffold(
+              body: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(tDefaultSize),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: 120,
+                        height: 120,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: const Image(
+                            image: AssetImage(tProfileImage),
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: tDefaultSize / 2),
-                    Text(
-                      snapshot.data!,
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                    const SizedBox(height: tDefaultSize / 2),
-                    SizedBox(
-                      width: 200,
-                      child: ElevatedButton(
-                        onPressed: () => Get.to(() => const UpdateProfileScreen()),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: tRedCroixRougeColor,
-                          side: BorderSide.none,
-                          shape: const StadiumBorder(),
-                        ),
-                        child: const Text(tEditProfile),
+                      const SizedBox(height: tDefaultSize / 2),
+                      Text(
+                        snapshot.data!,
+                        style: const TextStyle(fontSize: 20),
                       ),
-                    ),
-                    const SizedBox(height: tDefaultSize),
-                    const Divider(),
-                    const SizedBox(height: tDefaultSize / 2),
-                    ProfileMenuWidget(
-                      title: _isDark ? tLightMode : tDarkMode,
-                      icon: _isDark ? tSunIcon : tMoonIcon,
-                      onPress: () {},
-                      trailing: Switch(
-                        value: _isDark,
-                        onChanged: (value) {
-                          Get.changeThemeMode(
-                            value ? ThemeMode.dark : ThemeMode.light,
+                      const SizedBox(height: tDefaultSize / 2),
+                      SizedBox(
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: () => Get.to(() => const UpdateProfileScreen()),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: tRedCroixRougeColor,
+                            side: BorderSide.none,
+                            shape: const StadiumBorder(),
+                          ),
+                          child: const Text(tEditProfile),
+                        ),
+                      ),
+                      const SizedBox(height: tDefaultSize),
+                      const Divider(),
+                      const SizedBox(height: tDefaultSize / 2),
+                      ProfileMenuWidget(
+                        title: _isDark ? tLightMode : tDarkMode,
+                        icon: _isDark ? tSunIcon : tMoonIcon,
+                        onPress: () {},
+                        trailing: Switch(
+                          value: _isDark,
+                          onChanged: (value) {
+                            Get.changeThemeMode(
+                              value ? ThemeMode.dark : ThemeMode.light,
+                            );
+                            setState(() {
+                              _isDark = value;
+                            });
+                          },
+                        ),
+                      ),
+                      ProfileMenuWidget(
+                        title: tUsersPanel,
+                        icon: tUserPanelIcon,
+                        onPress: () => Get.to(() => const UserPanelScreen()),
+                        trailing: Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            color: tSecondaryColor.withOpacity(0.1),
+                          ),
+                          child: const Icon(tArrowRightIcon),
+                        ),
+                      ),
+                      const SizedBox(height: tDefaultSize / 2),
+                      const Divider(),
+                      const SizedBox(height: tDefaultSize / 2),
+                      ProfileMenuWidget(
+                        title: tInfo,
+                        icon: tInformationIcon,
+                        onPress: () {
+                          Get.defaultDialog(
+                            title: tInfo,
+                            middleTextStyle: TextStyle(
+                                fontSize: 14, color: _isDark ? tWhiteColor : tBlack),
+                            middleText: tInfoText,
+                            textConfirm: tOk,
+                            confirmTextColor: tWhiteColor,
+                            onConfirm: () => Get.back(),
                           );
-                          setState(() {
-                            _isDark = value;
-                          });
+                        },
+                        trailing: Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            color: tSecondaryColor.withOpacity(0.1),
+                          ),
+                          child: const Icon(tArrowRightIcon),
+                        ),
+                      ),
+                      ProfileMenuWidget(
+                        title: tLogout,
+                        titleColor: tRedCroixRougeColor,
+                        icon: tLogoutIcon,
+                        onPress: () {
+                          controller.logoutUser();
                         },
                       ),
-                    ),
-                    ProfileMenuWidget(
-                      title: tUsersPanel,
-                      icon: tUserPanelIcon,
-                      onPress: () => Get.to(() => const UserPanelScreen()),
-                      trailing: Container(
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: tSecondaryColor.withOpacity(0.1),
-                        ),
-                        child: const Icon(tArrowRightIcon),
-                      ),
-                    ),
-                    const SizedBox(height: tDefaultSize / 2),
-                    const Divider(),
-                    const SizedBox(height: tDefaultSize / 2),
-                    ProfileMenuWidget(
-                      title: tInfo,
-                      icon: tInformationIcon,
-                      onPress: () {
-                        Get.defaultDialog(
-                          title: tInfo,
-                          middleTextStyle: TextStyle(
-                              fontSize: 14, color: _isDark ? tWhiteColor : tBlack),
-                          middleText: tInfoText,
-                          textConfirm: tOk,
-                          confirmTextColor: tWhiteColor,
-                          onConfirm: () => Get.back(),
-                        );
-                      },
-                      trailing: Container(
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: tSecondaryColor.withOpacity(0.1),
-                        ),
-                        child: const Icon(tArrowRightIcon),
-                      ),
-                    ),
-                    ProfileMenuWidget(
-                      title: tLogout,
-                      titleColor: tRedCroixRougeColor,
-                      icon: tLogoutIcon,
-                      onPress: () {
-                        controller.logoutUser();
-                      },
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        } else if (snapshot.hasError) {
-          return const LoadingResultErrorWidget();
-        } else {
+            );
+          } else if (snapshot.hasError) {
+            return const LoadingResultErrorWidget();
+          } else {
+            return const LoadingResultErrorWidget();
+          }
+        }
+        else {
           return const LoadingResultWidget();
         }
       },
